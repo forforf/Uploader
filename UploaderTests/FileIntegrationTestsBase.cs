5a8bf2c6a6e41d7091ce114ad0d4738949df97c9
Dave M <dave@radiusnetworks.com>
2016-05-13 15:09:19 -0400

    Initial ObservableFileWatcher

---
+++ UploaderTests/FileIntegrationTestsBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace UploaderTests
+{
+    public class FileIntegrationTestsBase : AssertionHelper
+    {
+        protected string TempPath;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            var BasePath = @"c:\temp";
+            TempPath = BasePath + Guid.NewGuid().ToString();
+            Directory.CreateDirectory(TempPath);
+        }
+
+        [TearDown]
+        public void AfterEachTest()
+        {
+            if (!Directory.Exists(TempPath))
+            {
+                return;
+            }
+            Directory.Delete(TempPath, true);
+        }
+    }
+}
